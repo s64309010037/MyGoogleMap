99d4785f1e6b1b0eb4cd55d76a716b7b3e714030
s64309010037 <s64309010037@ptnicec.ac.th>
2022-11-01 17:59:36 +0700

    Map ส่งครั้งที่ 1

---
+++ src/pages/home/home.ts
@@ -0,0 +1,54 @@
+import { Component,ViewChild,ElementRef  } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { IonicPage } from 'ionic-angular';
+
+declare var google;
+
+@IonicPage()
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+  start = 'Muang Pattani';
+  end = 'Muang Pattani';
+  directionsService = new google.maps.DirectionsService;
+  directionsDisplay = new google.maps.DirectionsRenderer;
+
+  constructor(public navCtrl: NavController) {
+
+  }
+  ionViewDidLoad(){
+    this.initMap();
+  }
+
+  initMap() {
+    this.map = new google.maps.Map(this.mapElement.nativeElement, {
+      zoom: 7,
+      //center: {lat: 41.85, lng: -87.65}
+      center: {lat: 13.736717, lng: 100.523186}
+
+    });
+
+    this.directionsDisplay.setMap(this.map);
+  }
+
+  calculateAndDisplayRoute() {
+    this.directionsService.route({
+      origin: this.start,
+      destination: this.end,
+      travelMode: 'DRIVING'
+    }, (response, status) => {
+      if (status === 'OK') {
+        this.directionsDisplay.setDirections(response);
+      } else {
+        window.alert('Directions request failed due to ' + status);
+      }
+    });
+  }
+
+}
+
+
